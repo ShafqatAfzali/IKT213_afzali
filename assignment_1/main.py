373ecd6aba79c4d8af93662ae2555f356641501d
shefaghata <shefaghata@uia.no>
2025-08-24 04:22:23 +0200

    seccond commit

---
+++ assignment_1/main.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+
+def print_image_information(image):
+    my_img = cv.imread(image)
+    height, width, channels = my_img.shape
+    print("height:", height)
+    print("width:", width)
+    print("channels:", channels)
+    print("size:", my_img.size)
+    print("data type:", my_img.dtype)
+    return 0
+
+def SaveMyWeb(filepath):
+    webcam=cv.VideoCapture(0)
+    fps = webcam.get(cv.CAP_PROP_FPS)
+    width = int(webcam.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(webcam.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    with open(filepath, "a") as file:
+        file.write("fps: \t" + str(fps) + "\n")
+        file.write("width: \t" + str(width) + "\n")
+        file.write("height: \t" + str(height) + "\n")
+
+    webcam.release()
+
+
+
+print_image_information('lena-1.png')
+SaveMyWeb('camera_outputs.txt')
